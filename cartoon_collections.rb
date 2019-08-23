5c8d53429ba1e0954c592efff6541dbff42f7b87
Lessllie Real <rlessllie@yahoo.com>
2019-08-23 18:25:49 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
+def 
+  roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy", "Sneezy", "Sleepy", "Happy"]).each_with_index {|item| item, index}
+
+  # code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet([["earth", "wind", "fire", "water", "heart"]]).capitalize {|item| item, #{}!}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(short_words)
+  short_words = ["puff", "go", "two"]
+  assorted_words=["two", "go", "industrious", "bop"]
+  long_planeteer_calls(short_words).length
+  if short_words>4 
+    puts "longer than 4"
+  end
+  if long_planeteer_calls(assorted_words)
+    return true
+  end
+  if long_planeteer_calls(short_words)
+    return false
+  end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese
+  cheese= ["cheddar", "gouda", "camembert"]
+  snacks = ["crackers", "gouda", "thyme"]
+  soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+  find_the_cheese(["crackers", "gouda", "thyme"]).find {|cheese| cheese, index}
 end
